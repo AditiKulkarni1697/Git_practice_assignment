67dd0c645f48d6de9c7c4ba0f748438320cd8ea3
AditiKulkarni1697 <aditisujlegaonkar@gmail.com>
2022-11-03 16:20:04 +0530

    code changed again by Adi

--- primeNumber.js
+++ primeNumber.js
@@ -1,12 +1,12 @@
 
 let numBer = 13;
- 
+ let counter;
 for(i=2;i<=numBer**1/2;i++){
 if(numBer%i==0){
-factor++}
+counter++}
 }
 
-if(factor==1){
+if(counter==1){
 console.log("Not a prime")
 else{
 console.log("a prime)
